ce8d5ba22f49ed41c257d4f50d0704ef96306c90
Gringss <kggrings@arezzo.com.br>
2022-05-20 10:31:04 -0300

    Criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,12 +1,15 @@
 package br.com.dio;
 
+import jdk.swing.interop.SwingInterOpUtils;
 import model.Cachorro;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Cachorro cachorro = new Cachorro();
-
         System.out.println(cachorro);
+
+        Livro livro1 = new Livro("Seja foda", 200);
+        System.out.println(livro1);
 
        /*int a = 5;
        int b = 3;
@@ -14,5 +17,38 @@ public class PrimeiroPrograma {
         System.out.println("Hello World " + (a + b));*/
     }
 
+}
 
+class Livro {
+    private String name;
+    private Integer numPaginas;
+
+    public Livro(String name, Integer numPaginas) {
+        this.name = name;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "name='" + name + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
